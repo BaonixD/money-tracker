d5b82a27b238d8953ae327d95914eac8fd62c827
baonix <amzehanilias@gmail.com>
2026-03-27 23:03:17 +0500

    feat: add PATCH and DELETE endpoints for categories

--- backend/src/category/service.py
+++ backend/src/category/service.py
@@ -2,7 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.src.category.model import Category
-from backend.src.category.schemas import CategoryCreate
+from backend.src.category.schemas import CategoryCreate, CategoryUpdate
 
 
 async def create_category(db: AsyncSession, data: CategoryCreate) -> Category:
@@ -23,3 +23,27 @@ async def get_category_by_id(db: AsyncSession, category_id: int) -> Category | N
         select(Category).where(Category.id == category_id)
     )
     return result.scalar_one_or_none()
+
+async def update_category( db: AsyncSession, category_id: int, data: CategoryUpdate) -> Category| None:
+
+    category = await get_category_by_id(db, category_id)
+    if not category:
+        return None
+
+    updated_category = data.model_dump(exclude_unset=True)
+
+    for key, value in updated_category.items():
+        setattr(category, key, value)
+
+    await db.commit()
+    await db.refresh(category)
+    return category
+
+async def delete_category(db: AsyncSession, category_id: int) -> bool:
+    category = await get_category_by_id(db, category_id)
+    if not category:
+        return False
+    await db.delete(category)
+    await db.commit()
+    return True
+
