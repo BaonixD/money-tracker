b2e501307c90b222e40568d0904673839830845c
baonix <amzehanilias@gmail.com>
2026-03-27 20:08:30 +0500

    feat: add router and service to transaction

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,13 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from backend.database import engine, Base
 from backend.src.auth.router import router as auth_router
+from backend.src.category.router import router as category_router
+from backend.src.transactions.router import router as transaction_router
+
+# импорт моделей чтобы Base.metadata их увидел при create_all
 from backend.src.auth.model import User
+from backend.src.category.model import Category
+from backend.src.transactions.model import Transaction
 
 
 @asynccontextmanager
@@ -13,6 +19,8 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Money Tracker", lifespan=lifespan)
 app.include_router(auth_router)
+app.include_router(category_router)
+app.include_router(transaction_router)
 
 @app.get("/")
 def read_root():
